0fbeb89e3b6372327b7a27730f6e5146d9ebf01d
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-03 18:34:45 +0000

    updated App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-
-// import Navbar from "./components/Navbar";
+import React, {Component} from 'react'
+import Router from './Router'
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar */}
       <h1>Under Contruction</h1>
+      <Router />
     </div>
   );
 }
