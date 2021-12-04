307a4acaa67865b64b90835394e6e9e8704a4080
Ranffi <ranffiramirez@gmail.com>
2021-12-04 02:04:03 +0000

    created and styled HomePage component, added get route for the categories

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const {Cart, Category, Item, User} = require('./models')
 const express = require("express");
 const app = express();
+const cors = require('cors')
 // const { body, validationResult } = require("express-validator");
 const PORT = 3001;
-
+app.use(cors())
 const seed = require("./seed");
 
 app.use(express.json());
@@ -11,6 +12,16 @@ require("./models");
 
 //invoke our seed function
 seed();
+
+app.get('/categories', async(req, res, next) => {
+  try {
+    const categories = await Category.findAll()
+    res.send(categories)
+    console.log('we hit this route from the frontend')
+  } catch(err) {
+    next(err)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Your server is now listening to port: ${PORT}`);
