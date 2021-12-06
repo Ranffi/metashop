d8f86e9e5a0e45a12853473e3125b623884c9c40
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-06 22:32:48 +0000

    set up login and sign out

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,18 @@ import fire from '../fire'
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   
-    fire.auth().onAuthStateChanged((user) => {
+  fire.auth().onAuthStateChanged((user) => {
       return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
   });
+
+  const signOut = () => {
+    fire.auth().signOut()
+  };
 
   console.log('logged in?', isLoggedIn)
   return (
     <div className="App">
-      <Router />
+      <Router isLoggedin={isLoggedIn} signOut={signOut}/>
     </div>
   );
 }
