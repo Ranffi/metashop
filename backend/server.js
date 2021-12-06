076414f74f1e48b38f79365c2202069403d7e400
monterojavier <monterodotjavier@gmail.com>
2021-12-06 11:28:29 -0500

    backend cart

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const {Cart, Category, Item, User} = require('./models')
+const { Category, Item, User } = require("./models");
 const express = require("express");
 const app = express();
 var cors = require('cors')
@@ -16,61 +16,74 @@ app.use(cors())
 seed();
 
 // GET items
-app.get('/items', async (req, res, next) => {
+app.get("/items", async (req, res, next) => {
   const items = await Item.findAll();
-  res.json(items)
-})
+  res.json(items);
+});
 
 // GET single item
-app.get('/items/:itemId', async (req, res, next) => {
-
+app.get("/items/:itemId", async (req, res, next) => {
   try {
     const item = await Item.findOne({
       where: {
-        id: req.params.itemId
-      }
-    })
-    res.json(item)
-  
+        id: req.params.itemId,
+      },
+    });
+    res.json(item);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
+
+// GET cart
+app.get("/user/:id/cart", async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const cart = await User.findOne({
+      where: {
+        id: id,
+      },
+      include: [Item],
+    });
+
+    res.send(cart);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST/Create item
-app.post('/items', async (req, res, next) =>{
+app.post("/items", async (req, res, next) => {
   try {
     const item = await Item.create(req.body);
-    res.send(item)
-  
+    res.send(item);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //PUT/Update item
-app.put('/items/:itemId', async (req, res, next) =>{
+app.put("/items/:itemId", async (req, res, next) => {
   try {
-    const item = await Item.findByPk(req.params.itemId)
-    const updatedItem = await item.update(req.body)
-    res.json(updatedItem)
-  
+    const item = await Item.findByPk(req.params.itemId);
+    const updatedItem = await item.update(req.body);
+    res.json(updatedItem);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //DELETE/Destroy item
-app.delete('/items/:itemId', async (req, res, next) =>{
+app.delete("/items/:itemId", async (req, res, next) => {
   try {
-    const item = await Item.findByPk(req.params.itemId)
-    const destroyedItem = await item.destroy()
-    res.json(destroyedItem)
+    const item = await Item.findByPk(req.params.itemId);
+    const destroyedItem = await item.destroy();
+    res.json(destroyedItem);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
-
+});
 
 app.listen(PORT, () => {
   console.log(`Your server is now listening to port: ${PORT}`);
