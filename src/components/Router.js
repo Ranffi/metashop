252589f512ba51b90e4b9c838109762dbd0be219
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-07 17:22:26 +0000

    updated Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
-import Items from "./itemsPage";
+import Items from "./ItemsPage";
 import Homepage from "./HomePage";
 import Login from "./sessions/Login";
 import CartPage from "./CartPage";
-import SingleItem from "./singleItemPage";
+import SingleItem from "./SingleItemPage";
 
 const Router = ({ isLoggedin, signOut }) => {
   return (
