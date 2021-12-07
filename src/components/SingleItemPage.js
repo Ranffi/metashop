685bd182edb86297b2ff884e5bd70d7cf22a0d3d
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2021-12-07 21:11:59 +0000

    changed functions to arrow functions so that we don't have to bind

--- src/components/SingleItemPage.js
+++ src/components/SingleItemPage.js
@@ -6,55 +6,49 @@ import {Flex, Box, Image,} from '@chakra-ui/react';
 const url = "http://localhost:3001";
 
 class SingleItem extends Component {
-  constructor() {
-    super()
-    this.state = {
-      item: {}
-    }
-    this.onDelete = this.onDelete.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+  state = {
+    item: {}
   }
 
-  componentDidMount(){
-      axios.get(`${url}/items/${window.location.pathname.split('/')[2]}`)
-      .then(res => {
-        this.setState({item: res.data});
-      })        
+  componentDidMount() {
+    axios.get(`${url}/items/${window.location.pathname.split('/')[2]}`)
+    .then(res => {
+      this.setState({item: res.data});
+    })        
   }
 
-  onDelete(){
-      axios.delete(`${url}/items/${window.location.pathname.split('/')[2]}`)
-      .then(res => {
-          this.setState({item: res.data});
-        }) 
+  onDelete = () => {
+    axios.delete(`${url}/items/${window.location.pathname.split('/')[2]}`)
+    .then(res => {
+      this.setState({item: res.data});
+    }) 
   }
 
-  handleUpdate(event){
-      event.preventDefault()
-      const title = event.target.titleOfItem.value
-      const imageUrl = event.target.imageOfItem.value
-      const description = event.target.descriptionOfItem.value
-      const price = event.target.priceOfItem.value
-    
-      axios.put(`${url}/items/${window.location.pathname.split('/')[2]}`,{
-        title,
-        imageUrl,
-        description,
-        price,
-      })
-      .then(res => {
-          this.setState({item: res.data});
-        })  
+  handleUpdate = (event) => {
+    event.preventDefault()
+    const title = event.target.titleOfItem.value
+    const imageUrl = event.target.imageOfItem.value
+    const description = event.target.descriptionOfItem.value
+    const price = event.target.priceOfItem.value
+  
+    axios.put(`${url}/items/${window.location.pathname.split('/')[2]}`,{
+      title,
+      imageUrl,
+      description,
+      price,
+    })
+    .then(res => {
+      this.setState({item: res.data});
+    })  
   }
 
-  handleChange(event){
+  handleChange = (event) => {
       this.setState({
         [event.target.name]: event.target.value
       })
   }
 
-  render(){
+  render() {
       const item = this.state.item
       return(
         <div>
@@ -101,50 +95,50 @@ class SingleItem extends Component {
           </Flex>
           </div>
           <div>
-                  <h1>Update Item</h1>
-              <form onSubmit={this.handleUpdate}>
-                    <label>
-                      Title of Item: 
-                      <input
-                        type="text"
-                        name="titleOfItem"
-                        onChange={this.handleChange}
-                      />
-                    </label>
-  
-                    <label>
-                      Image of Item: 
-                      <input
-                        type="text"
-                        name="imageOfItem"
-                        onChange={this.handleChange}
-                      />
-                    </label>
-  
-                    <label>
-                      Description of Item: 
-                      <input
-                        type="text"
-                        name="descriptionOfItem"
-                        onChange={this.handleChange}
-                      />
-                    </label>
-  
-                    <label>
-                      Price of Item: 
-                      <input
-                        type="text"
-                        name="priceOfItem"
-                        onChange={this.handleChange}
-                      />
-                    </label>
-  
-                    <button type="submit">Submit</button>
-                  </form>
-              </div>
-         </div>
+            <h1>Update Item</h1>
+            <form onSubmit={this.handleUpdate}>
+              <label>
+                Title of Item: 
+                <input
+                  type="text"
+                  name="titleOfItem"
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label>
+                Image of Item: 
+                <input
+                  type="text"
+                  name="imageOfItem"
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label>
+                Description of Item: 
+                <input
+                  type="text"
+                  name="descriptionOfItem"
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label>
+                Price of Item: 
+                <input
+                  type="text"
+                  name="priceOfItem"
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
       )
     }
   }
-  
+
   export default SingleItem
