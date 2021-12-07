5893e15b2cca29f0d77b22236d33410341c57b53
monterojavier <monterodotjavier@gmail.com>
2021-12-06 20:08:38 -0500

    Small tweak to login page

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,45 +1,35 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'
-import Navbar from './Navbar'
-import Items from "./itemsPage"
-import Homepage from './homePage'
-import Login from './sessions/Login'
-// import homePage from './cartPage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Items from "./ItemsPage";
+import Homepage from "./HomePage";
+import Login from "./sessions/Login";
+import CartPage from "./CartPage";
 
-const Router = ({isLoggedin, signOut}) => {
-    return (
+const Router = ({ isLoggedin, signOut }) => {
+  return (
+    <>
+      <Navbar signOut={signOut} isLoggedin={isLoggedin} />
+      {!isLoggedin ? (
         <>
-            <Navbar />
-            {!isLoggedin ? (
-            <>
-                <Routes>
-                    <Route path="/" element={<Login />}>Log In</Route>
-                </Routes>
-            </>
-            ) : (
-                <>
-                    <Routes>
-                        <Route path='/' element={<Homepage/>} />
-                        {/* <Route path='/cart' element={<cartPage/>} /> */}
-                        {/* <Route path='/' element={<homePage/>} />
-                        <Route path='/cart' element={<cartPage/>} /> */}
-                        <Route path="/categories/:id/items" element={<Items />} />
-                    </Routes>
-                    <span onClick={signOut}>
-                        <a href="http://localhost:3000/#">Sign out</a>
-                    </span>
-                </>
-            )}
+          <Routes>
+            <Route path="/" element={<Login />}>
+              Log In
+            </Route>
+          </Routes>
         </>
-    );
-}
+      ) : (
+        <>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/" element={<homePage />} />
+            <Route path="/categories/:id/items" element={<Items />} />
+          </Routes>
+        </>
+      )}
+    </>
+  );
+};
 
 export default Router;
-
-{/* <Routes> */}
-    {/* <Route path='/' element={<HomePage/>} /> */}
-    {/* <Route path='/cart' element={<cartPage/>} /> */}
-    {/* <Route path='/' element={<homePage/>} />
-    <Route path='/cart' element={<cartPage/>} /> */}
-    {/* <Route path="/items" element={<Items />} /> */}
-{/* </Routes> */}
