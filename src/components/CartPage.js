7ccfca07cbd0e2fb9930e28ea435ace114e041b8
monterojavier <monterodotjavier@gmail.com>
2021-12-09 11:59:43 -0500

    change format to single items page

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -13,16 +13,16 @@ import {
   Tr,
   Td,
   Th,
+  Flex,
   Button as ChakraButton,
 } from "@chakra-ui/react";
 import { MdPayment } from "react-icons/md";
-import {FaTrash } from "react-icons/fa"
+import { FaTrash } from "react-icons/fa";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const url = "http://localhost:3001";
-
 
 class CartPage extends Component {
   state = {
@@ -44,55 +44,58 @@ class CartPage extends Component {
 
   async handleRemoveFromCart(itemId) {
     await axios
-    .delete(
-      `${url}/items/${itemId}/user/${this.props.userEmail}/cart`
-
-    )
-    .then(() =>
-
+      .delete(`${url}/items/${itemId}/user/${this.props.userEmail}/cart`)
+      .then(() =>
         toast.error("Removed from Cart", {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: true,
-        draggable: true,
-      })
-    );
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: true,
+          draggable: true,
+        })
+      );
 
     const cart = await axios.get(`${url}/user/${this.props.userEmail}/cart`);
     this.setState({ itemsInCart: [...cart.data.Items] });
 
     let total = 0;
 
-    this.state.itemsInCart.forEach((item) => ( total += item.price));
+    this.state.itemsInCart.forEach((item) => (total += item.price));
     this.setState({ totalPrice: total });
   }
 
-  handleCheckoutButtonClick(){
-    toast.info("Checkout does not work, but you can give us money if they want 🤡", {
-      position: "top-right",
-      autoClose: 6000,
-      hideProgressBar: true,
-      draggable: true,
-    })
+  handleCheckoutButtonClick() {
+    toast.info(
+      "Checkout does not work, but you can give us money if they want 🤡",
+      {
+        position: "top-right",
+        autoClose: 6000,
+        hideProgressBar: true,
+        draggable: true,
+      }
+    );
   }
 
   render() {
     return (
-      <Container maxW={"7xl"} >
+      <Container maxW={"7xl"}>
         <Divider paddingTop="5" paddingBottom="20" />
 
         <Heading size="lg" fontSize="60px">
           Your Cart
         </Heading>
         <Box
-
           paddingTop={{ base: "1", sm: "5" }}
           display="flex"
           flexDirection={{ base: "column", sm: "row" }}
           justifyContent="space-between"
           alignItems="flex-start"
         >
-          <Box flex="4" paddingRight="3" position="relative" alignItems="center">
+          <Box
+            flex="4"
+            paddingRight="3"
+            position="relative"
+            alignItems="center"
+          >
             <Table>
               <Thead>
                 <Tr>
@@ -110,22 +113,29 @@ class CartPage extends Component {
                   return (
                     <Tr key={item.id}>
                       <Td>
-                        <Image
-                          src={item.image}
-                          boxSize="4em"
-                          borderRadius="0.5em"
-                          marginBottom="5"
-                        />
+                        <Center>
+                          <Image
+                            src={item.image}
+                            boxSize="4em"
+                            borderRadius="0.5em"
+                            marginBottom="5"
+                          />
+                        </Center>
                       </Td>
                       <Td>
                         <Text flex="1">{item.title}</Text>
                       </Td>
                       <Td>
                         <Center>
-                          <Text>{`$${item.price}`}</Text>
-                          <Container>
-                          <FaTrash onClick={()=>this.handleRemoveFromCart(item.id)}/>
-                          </Container>
+                          <Flex direction="row" align="center">
+                            <Text>{`$${item.price} `}</Text>
+                            <ChakraButton
+                              bg="none"
+                              onClick={() => this.handleRemoveFromCart(item.id)}
+                            >
+                              <FaTrash />
+                            </ChakraButton>
+                          </Flex>
                         </Center>
                       </Td>
                     </Tr>
@@ -163,7 +173,11 @@ class CartPage extends Component {
                     marginBottom="40"
                   >{`$${this.state.totalPrice}`}</Text>
                 </Center>
-                <ChakraButton onClick={this.handleCheckoutButtonClick} bg="#9A8C98" rightIcon={<MdPayment />}>
+                <ChakraButton
+                  onClick={this.handleCheckoutButtonClick}
+                  bg="#9A8C98"
+                  rightIcon={<MdPayment />}
+                >
                   Checkout
                 </ChakraButton>
               </Box>
